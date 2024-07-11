1cec140d114cae035c07c498a288cc9ffe4096b3
Miguel Julio Rodo <miguel.rodo@uct.ac.za>
2024-07-11 09:23:56 +0000

    Delete unused R scripts

--- R/import.R
+++
@@ -1,7 +0,0 @@
-#' @importFrom tidyr gather separate spread
-#' @import stringr
-#' @importFrom magrittr %<>% %>%
-#' @importFrom openxlsx read.xlsx
-#' @importFrom tibble tibble as_tibble
-#' @import dplyr
-NULL
