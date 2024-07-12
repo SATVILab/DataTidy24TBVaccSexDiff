7d0926f93cfb6288cb3ad59695da898afffa2cae
Miguel Julio Rodo <miguel.rodo@uct.ac.za>
2024-07-12 13:44:55 +0000

    Add corrected data, calc and extraction functions and comparison plots

--- R/calc.R
+++ R/calc.R
@@ -3,10 +3,18 @@
 # =====================================
 
 #' @title Calculate the vaccine-induced response
+#'
+#' @description
+#' To calculate the vaccine-induced response, we subtract the baseline response.
+#' If the baseline response is missing for a particular individual,
+#' we subtract the
+#' median baseline response for that individual's vaccine and
+#' infection status group.
+#'
 #' @export
-calc_vaccine_induced_response <- function(.data,
-                                          cn_resp,
-                                          grp = NULL) {
+vacc_calc_vaccine_induced_response <- function(.data,
+                                               cn_resp,
+                                               grp = NULL) {
   # find the value to actually subtract,
   # using the day zero value if available but the median value if not
   .data <- .calc_day_zero_to_subtract(.data, cn_resp = cn_resp, grp = grp)
@@ -71,4 +79,69 @@ calc_vaccine_induced_response <- function(.data,
     )
   }
   grp[grp %in% colnames(.data)]
+}
+
+#' @title Set negative responses to zero
+#'
+#' @param .data A data frame.
+#' @cn_resp character.
+#' Column name in `.data` that contains the response.
+#'
+#' @export
+vacc_set_neg_to_zero <- function(.data, cn_resp) {
+  .data[, cn_resp] <- pmax(.data[[cn_resp]], 0)
+  .data
+}
+
+#' @rdname calc_response_transformation
+#' @title Calculate the summed and profile response
+#'
+#' @description
+#' The functions `calc_response_summed` and `calc_response_profile`
+#' calculate the summed and profile response, respectively,
+#' as defined in Rodo (2019).
+#'
+#' @param .data A data frame.
+#' @param cn_resp character.
+#' Column name in `.data` that contains the response.
+#' @param grp character vector.
+#' Columns in `.data` containing the grouping variable.
+#' If `NULL` (the default), then these are automatically determined.
+#'
+#' @export
+vacc_calc_response_summed <- function(.data, cn_resp, grp = NULL) {
+  grp_vec <- .get_grp_vec(.data, grp = grp) |> setdiff("cyt_combn")
+  .data <- .data |>
+    dplyr::group_by(dplyr::across(grp_vec)) |>
+    dplyr::summarise(
+      response = sum(.data[[cn_resp]], na.rm = TRUE),
+      .groups = "drop"
+    )
+  if (cn_resp == "response") {
+    return(.data)
+  }
+  .data[, cn_resp] <- .data[["response"]]
+  .data |> dplyr::select(-response) # nolint
+}
+
+#' @rdname calc_response_transformation
+#' @export
+vacc_calc_response_profile <- function(.data, cn_resp, grp = NULL) {
+  grp_vec <- .get_grp_vec(.data, grp = grp) |> setdiff("cyt_combn")
+  .data |>
+    dplyr::group_by(dplyr::across(grp_vec)) |>
+    dplyr::mutate(
+      response_abs_sum = sum(abs(.data[[cn_resp]]), na.rm = TRUE),
+      response_abs_sum = ifelse(
+        response_abs_sum == 0, NA_real_, response_abs_sum # nolint
+      ),
+      response = .data[[cn_resp]] / response_abs_sum
+    ) |>
+    dplyr::ungroup() |>
+    dplyr::select(-response_abs_sum)
+  if (cn_resp == "response") {
+    return(.data)
+  }
+  .data[, cn_resp] <- .data[["response"]]
+  .data |> dplyr::select(-response) # nolint
 }
---
+++ R/extract.R
@@ -0,0 +1,82 @@
+#' @rdname extract_timepoint
+#' @title Extract data from particular timepoints
+#'
+#' @description
+#' These functions extract data from particular timepoints.
+#' The functions `vacc_extract_baseline`, `vacc_extract_peak`, and
+#' `vacc_extract_memory` extract the baseline, peak, and memory
+#' response, respectively.
+#' The timepoints are specified in the `peak_time` and `mem_time`
+#' arguments for the `vacc_extract_peak` and `vacc_extract_memory`
+#' functions, respectively.
+#'
+#' @param .data A data frame.
+#' @param cn_time character.
+#' Column name in `.data` that contains the timepoint.
+#' @param peak_time named numeric vector.
+#' Named vector specifying the peak timepoint for each vaccine.
+#' Names are the protocol names, and values are the peak timepoints.
+#' Must be supplied
+#' @param mem_time named numeric vector.
+#' Named vector specifying the memory timepoint for each vaccine.
+#' Names are the protocol names, and values are the memory timepoints.
+#' If not supplied, then the defaults are used.
+#'
+#' @export
+vacc_extract_baseline <- function(.data, cn_time = "timepoint") {
+  # extract the baseline response
+  .data |>
+    dplyr::filter(.data[[cn_time]] == 0)
+}
+
+#' @rdname extract_timepoint
+#' @export
+vacc_extract_peak <- function(.data,
+                              cn_time = "timepoint",
+                              peak_time = NULL) {
+  # extract the peak response
+  peak_time <- .vacc_extract_peak_peak_time(peak_time)
+  .data |>
+    dplyr::mutate(peak_time = peak_time[.data[["prid"]]]) |>
+    dplyr::filter(.data[[cn_time]] == .data[["peak_time"]]) |>
+    dplyr::select(-peak_time)
+}
+
+.vacc_extract_peak_peak_time <- function(peak_time) {
+  if (!is.null(peak_time)) {
+    return(peak_time)
+  }
+  c(
+    "040" = 70,
+    "c-035-456" = 70,
+    "tb008" = 7, # check
+    "tb011" = 7, # check
+    "thyb04" = 70
+  )
+}
+
+#' @rdname extract_timepoint
+#' @export
+vacc_extract_memory <- function(.data,
+                                cn_time = "timepoint",
+                                mem_time = NULL) {
+  # extract the memory response
+  mem_time <- .vacc_extract_memory_mem_time(mem_time)
+  .data |>
+    dplyr::mutate(mem_time = mem_time[.data[["prid"]]]) |>
+    dplyr::filter(.data[[cn_time]] == .data[["mem_time"]]) |>
+    dplyr::select(-mem_time)
+}
+
+.vacc_extract_memory_mem_time <- function(mem_time) {
+  if (!is.null(mem_time)) {
+    return(mem_time)
+  }
+  c(
+    "040" = 999,
+    "c-035-456" = 292,
+    "tb008" = 168,
+    "tb011" = 364,
+    "thyb04" = 224
+  )
+}
